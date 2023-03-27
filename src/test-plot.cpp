9befa95f118e5368ed24bcf5ff0f78365cdbd21f
jsvirzi <jsvirzi>
2023-03-27 03:20:54 -0700

    testing. using 2 graphs

--- src/test-plot.cpp
+++ src/test-plot.cpp
@@ -44,29 +44,37 @@ int main(int argc, char **argv)
     int mask = n_x - 1;
     double *graph_x = new double[16];
     double *graph_y = new double[16];
+    double latest_x[1];
+    double latest_y[1];
 
     pthread_t tid;
 
     pthread_create(&tid, NULL, app_looper, 0);
 
     // TF1 *f1 = new TF1("f1","sin(x)", x, x + 10);
-    TGraph *graph = new TGraph(n_x, graph_x, graph_y);
-    graph->SetName("graph");
-    graph->SetMarkerColor(kBlue);
-    graph->SetMarkerStyle(kOpenCircle);
-    graph->SetMinimum(0.0);
-    graph->SetMaximum(1.0);
-    // graph->GetXaxis()->SetRangeUser(-1.25, 1.25);
-    graph->GetXaxis()->SetLimits(-1.25, 1.25);
+    TGraph *g_history = new TGraph(n_x, graph_x, graph_y);
+    g_history->SetName("g_history");
+    g_history->SetMarkerColor(kBlue);
+    g_history->SetMarkerStyle(kOpenCircle);
+    g_history->SetMinimum(0.0);
+    g_history->SetMaximum(1.0);
+    // g_history->GetXaxis()->SetRangeUser(-1.25, 1.25);
+    g_history->GetXaxis()->SetLimits(-1.25, 1.25);
+
+    latest_y[0] = 0.5;
+    TGraph *g_newest = new TGraph(1, latest_x, latest_y);
+    g_newest->SetMarkerStyle(kFullCircle);
 
     int i = 1;
     while (1)
     {
 
         if ((i % 2) == 0) {
-            graph->SetMarkerColor(kBlue);
+            g_history->SetMarkerColor(kBlue);
+            g_newest->SetMarkerColor(kBlue);
         } else {
-            graph->SetMarkerColor(kRed);
+            g_history->SetMarkerColor(kRed);
+            g_newest->SetMarkerColor(kRed);
         }
 
         for (int i = 0; i < n_x; ++i) {
@@ -77,14 +85,18 @@ int main(int argc, char **argv)
 
         x = x + 0.1;
 
+        latest_x[0] = graph_x[0];
+        g_newest->SetPoint(0, latest_x[0], latest_y[0]);
+
         for (int i = 0; i < n_x; ++i) {
-            graph->SetPoint(i, graph_x[i], graph_y[i]);
+            g_history->SetPoint(i, graph_x[i], graph_y[i]);
         }
 
-        // graph->GetXaxis()->SetRangeUser(-1.25, 1.25);
-        graph->GetXaxis()->SetLimits(-1.25, 1.25);
+        // g_history->GetXaxis()->SetRangeUser(-1.25, 1.25);
+        g_history->GetXaxis()->SetLimits(-1.25, 1.25);
 
-        graph->Draw("ap");
+        g_history->Draw("ap");
+        g_newest->Draw("p");
 
         c->Modified();
         c->Update();
@@ -103,7 +115,7 @@ int main(int argc, char **argv)
     TCanvas* c = new TCanvas("c", "Something", 0, 0, 800, 600);
     TF1 *f1 = new TF1("f1","sin(x)", -5, 5);
     f1->SetLineColor(kBlue+1);
-    f1->SetTitle("My graph;x; sin(x)");
+    f1->SetTitle("My g_history;x; sin(x)");
     f1->Draw();
     c->Print("demo1.pdf");
 #endif
