557e6c6c0f1dd8d473587db2a0f9ef7870aff473
jsvirzi <jsvirzi>
2023-03-27 02:55:04 -0700

    testing. cutting in graph

--- src/test-plot.cpp
+++ src/test-plot.cpp
@@ -23,14 +23,9 @@ void *app_looper(void *argv)
     while (1) {
         sleep(1);
         printf("iter = %d\n", ++iter);
-        if (iter > 5) {
+        if (iter >= 1) {
             g_text = new TText(0.0, 0.0, "hello");
             g_text->Draw();
-//            g_text->SetName("blah");
-//            if (g_text->GetName() != "lat") {
-//                g_text->SetName("lat");
-//                g_text->Draw();
-//            }
         }
     }
 
@@ -44,29 +39,57 @@ int main(int argc, char **argv)
     TCanvas* c = new TCanvas("c", "Something", 0, 0, 800, 600);
 
     double x = -5.0;
+    int n_x = 16;
+    int head = 0;
+    int mask = n_x - 1;
+    double *graph_x = new double[16];
+    double *graph_y = new double[16];
 
     pthread_t tid;
 
     pthread_create(&tid, NULL, app_looper, 0);
 
-    TF1 *f1 = new TF1("f1","sin(x)", x, x + 10);
-    f1->Draw("alp");
+    // TF1 *f1 = new TF1("f1","sin(x)", x, x + 10);
+    TGraph *graph = new TGraph(n_x, graph_x, graph_y);
+    graph->SetName("graph");
+    graph->SetMarkerColor(kBlue);
+    graph->SetMarkerStyle(kOpenCircle);
+    graph->SetMinimum(0.0);
+    graph->SetMaximum(1.0);
+    // graph->GetXaxis()->SetRangeUser(-1.25, 1.25);
+    graph->GetXaxis()->SetLimits(-1.25, 1.25);
+
     int i = 1;
     while (1)
     {
+
         if ((i % 2) == 0) {
-            f1->SetLineColor(kBlue+1);
-            f1->SetMarkerColor(kBlue + 1);
+            graph->SetMarkerColor(kBlue);
         } else {
-            f1->SetLineColor(kRed);
-            f1->SetMarkerColor(kRed);
+            graph->SetMarkerColor(kRed);
         }
-        f1->SetTitle("My graph;x; sin(x)");
-        f1->Draw("lp");
+
+        for (int i = 0; i < n_x; ++i) {
+            double phase = x + 0.1;
+            graph_x[i] = sin(phase + i * 0.1);
+            graph_y[i] = 0.5;
+        }
+
+        x = x + 0.1;
+
+        for (int i = 0; i < n_x; ++i) {
+            graph->SetPoint(i, graph_x[i], graph_y[i]);
+        }
+
+        // graph->GetXaxis()->SetRangeUser(-1.25, 1.25);
+        graph->GetXaxis()->SetLimits(-1.25, 1.25);
+
+        graph->Draw("ap");
+
         c->Modified();
         c->Update();
         c->Draw();
-        // c->WaitPrimitive("lat", "Text");
+
         c->WaitPrimitive();
         ++i;
         iter = 0;
@@ -76,20 +99,6 @@ int main(int argc, char **argv)
     rc->Connect("CloseWindow()", "TApplication", gApplication, "Terminate()");
     app.Run();
 
-    while (1)
-    {
-        f1->SetLineColor(kBlue+1);
-        f1->SetMarkerColor(kBlue + 1);
-        f1->SetTitle("My graph;x; sin(x)");
-        f1->Draw("alp");
-        c->Modified();
-        c->Update();
-        c->Draw();
-    }
-
-
-    // TGraph *graph = new TGraph();
-    // grap
 #if 0
     TCanvas* c = new TCanvas("c", "Something", 0, 0, 800, 600);
     TF1 *f1 = new TF1("f1","sin(x)", -5, 5);
