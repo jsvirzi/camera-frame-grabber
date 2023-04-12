dfeb4c6df3d7e2d19371e5f71edf8f1dc52a94df
jsvirzi <jsvirzi@gmail.com>
2023-04-12 16:14:51 -0700

    launch thread for histogram

--- src/graph-focus.cpp
+++ src/graph-focus.cpp
@@ -24,19 +24,46 @@ extern "C" {
         int n_focus;
         TApplication *app;
         TCanvas *canvas;
+        pthread_t thread_id;
+        int run;
     } FocusGraphInfo;
 
     static FocusGraphInfo focus_graph_info;
 
+    void *graph_focus_looper(void *arg)
+    {
+        FocusGraphInfo *info = (FocusGraphInfo *) arg;
+        while (info->run) {
+            sleep(1);
+
+            info->focus_hist_max->FillRandom("gaus", 10000);
+
+            TCanvas *c = info->canvas;
+
+            c->cd();
+
+            info->focus_hist_max->Draw();
+            info->focus_hist_max->Draw("same p");
+            info->focus_hist->Draw("same p");
+
+            c->Modified();
+            c->Update();
+            c->Draw();
+
+        }
+    }
+
     int initialize_focus_graph(int n_focus, int *argc, char **argv)
     {
         FocusGraphInfo *info = &focus_graph_info;
+        memset(&focus_graph_info, 0, sizeof (focus_graph_info));
+
         info->app = new TApplication("app", argc, argv);
         // TApplication app("app", &argc, argv);
         info->canvas = new TCanvas("c", "focus-analysis", 0, 0, 800, 600);
 
         info->focus_hist = new TH1D("focus-hist", "FOCUS", n_focus, -0.5, n_focus + 0.5);
-        info->focus_hist_max = new TH1D("focus-hist", "FOCUS", n_focus, -0.5, n_focus + 0.5);
+        info->focus_hist_max = new TH1D("focus-hist-max", "FOCUS", n_focus, -0.5, n_focus + 0.5);
 
         TH1D *h = info->focus_hist_max;
 
@@ -73,7 +100,10 @@ extern "C" {
         c->Update();
         c->Draw();
 
-        info->app->Run(kTRUE);
+        info->run = 1;
+        pthread_create(&info->thread_id, NULL, graph_focus_looper, info);
+
+        info->app->Run();
     }
 
     int display_focus_graph(double *focus)
