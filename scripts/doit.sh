66dfd503bb2893f9a232c7e3d17a52bfe754bbb7
jsvirzi <jsvirzi@gmail.com>
2023-04-17 01:19:11 -0700

    startup script for automatic execution

--- scripts/doit.sh
+++ scripts/doit.sh
@@ -1,3 +1,3 @@
 cd /mnt/data/jsvirzi/projects/camera-frame-grabber/build
-v4l-mmap -d /dev/video3 -res 1600 1300 -fmt 59455247 -name ${1}
+./v4l-mmap -d /dev/video3 -res 1600 1300 -fmt 59455247 -name ${1}
 
